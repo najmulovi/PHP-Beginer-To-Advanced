52d36daf3578f098446316fcbd42f4f440cccaee
mdnajmul <mdnajmulovi@gmail.com>
2021-07-05 17:02:45 +0600

    spaceship operator in php

---
+++ _1_php_basic/_17_Spaceship_operator.php
@@ -0,0 +1,18 @@
+<?php
+/*
+    ## The <=> ('Spaceship') operator will offer combined comparision in that it will:
+
+        Return 0 if values on either side are equal
+        Return 1 if the value on the left is greater
+        Return -1 if the value on the right is greater
+
+        //Comparing Integers:
+            echo 1 <=> 1; // output 0
+            echo 3 <=> 4; // output -1
+            echo 4 <=> 3; // output 1
+
+        //String Comparison
+            echo "x" <=> "x"; // output 0
+            echo "x" <=> "y"; // output -1
+            echo "y" <=> "x"; // output 1
+*/
